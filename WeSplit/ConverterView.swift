3697464a192341822b2a9c124ee6efaa5255e882
BarisCiftci <barishciftci@gmail.com>
2023-12-29 18:43:42 +0100

    NO_TICKET: Challenge: create  converter app add to WeSplit app

---
+++ WeSplit/ConverterView.swift
@@ -0,0 +1,18 @@
+//
+//  ConverterView.swift
+//  WeSplit
+//
+//  Created by Baris Ciftci on 25/12/2023.
+//
+
+import SwiftUI
+
+struct ConverterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConverterView()
+}
